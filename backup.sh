e895ac090a83b0fc786bd7847b73d545d236d1e0
jacobjc <jacobjchristiansen@gmail.com>
2019-06-01 17:37:56 -0400

    Creation

---
+++ backup.sh
@@ -0,0 +1,7 @@
+# Git: add
+cd /root/minecraftbe && /root/minecraftbe/git add "Debug_Log.txt" && /root/minecraftbe/git add "worlds"
+# commit changes
+cd /root/minecraftbe && /root/minecraftbe/git commit -m "Bi-Daily Backup `date`"
+# send data to Git server
+cd /root/minecraftbe && /root/minecraftbe/git push origin master
+
