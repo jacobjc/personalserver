798953889ba179b424541c42e7b9a6206d0552ed
jacobjc <jacobjchristiansen@gmail.com>
2019-06-01 16:12:16 -0400

    Creation

---
+++ start.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# James Chambers - March 7th 2019
+# Minecraft Bedrock server startup script using screen
+
+# Check if server is already started
+if screen -list | grep -q "minecraftbe"; then
+    echo "Server is already started!  Press screen -r minecraftbe to open it"
+    exit 1
+fi
+
+# Check if network interfaces are up
+NetworkChecks=0
+DefaultRoute=$(route -n | awk '$4 == "UG" {print $2}')
+while [ -z "$DefaultRoute" ]; do
+    echo "Network interface not up, will try again in 1 second";
+    sleep 1;
+    DefaultRoute=$(route -n | awk '$4 == "UG" {print $2}')
+    NetworkChecks=$((NetworkChecks+1))
+    if [ $NetworkChecks -gt 20 ]; then
+        echo "Waiting for network interface to come up timed out - starting server without network connection ..."
+        break
+    fi
+done
+
+# Change directory to server directory
+cd /root/minecraftbe/
+
+# Create backup
+if [ -d "worlds" ]; then
+    echo "Backing up server (to minecraftbe/backups folder)"
+    tar -pzvcf backups/$(date +%Y.%m.%d.%H.%M.%S).tar.gz worlds
+fi
+
+# Retrieve latest version of Minecraft Bedrock dedicated server
+echo "Checking for the latest version of Minecraft Bedrock server ..."
+
+# Test internet connectivity first
+wget --spider --quiet https://minecraft.net/en-us/download/server/bedrock/
+if [ "$?" != 0 ]; then
+    echo "Unable to connect to update website (internet connection may be down).  Skipping update ..."
+else
+    # Download server index.html to check latest version
+    wget -O downloads/version.html https://minecraft.net/en-us/download/server/bedrock/
+    DownloadURL=$(grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*' downloads/version.html)
+    DownloadFile=$(echo "$DownloadURL" | sed 's#.*/##')
+
+    # Download latest version of Minecraft Bedrock dedicated server if a new one is available
+    if [ -f "downloads/$DownloadFile" ]
+    then
+        echo "Minecraft Bedrock server is up to date..."
+    else
+        echo "New version $DownloadFile is available.  Updating Minecraft Bedrock server ..."
+        wget -O "downloads/$DownloadFile" "$DownloadURL"
+        unzip -o "downloads/$DownloadFile" -x "*server.properties*" "*permissions.json*" "*whitelist.json*"
+    fi
+fi
+
+echo "Starting Minecraft server.  To view window type screen -r minecraftbe"
+echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
+screen -dmS minecraftbe /bin/bash -c "LD_LIBRARY_PATH=/root/minecraftbe /root/minecraftbe/bedrock_server"
---
+++ stop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# James Chambers - March 2nd 2019
+# Minecraft Server stop script - primarily called by minecraft service but can be ran manually
+
+# Check if server is running
+if ! screen -list | grep -q "minecraftbe"; then
+  echo "Server is not currently running!"
+  exit 1
+fi
+
+# Stop the server
+echo "Stopping Minecraft server ..."
+screen -Rd minecraftbe -X stuff "say Closing server (stop.sh called)...$(printf '\r')"
+screen -Rd minecraftbe -X stuff "stop$(printf '\r')"
+
+# Wait up to 20 seconds for server to close
+StopChecks=0
+while [ $StopChecks -lt 20 ]; do
+  if ! screen -list | grep -q "minecraftbe"; then
+    break
+  fi
+  sleep 1;
+  StopChecks=$((StopChecks+1))
+done
+
+# Force quit if server is still open
+if screen -list | grep -q "minecraftbe"; then
+  echo "Minecraft server still hasn't closed after 20 seconds, closing screen manually"
+  screen -S minecraftbe -X quit
+fi
+
+echo "Minecraft server stopped."
